42a47c0e956bd19d330e9472ef9289399232794b
yoh <yokeus2000@gmail.com>
2024-12-20 22:12:23 +0900

    initial commit

---
+++ components/ImageViewer.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+import { Image, type ImageSource } from "expo-image";
+
+type Props = {
+  imgSource: ImageSource;
+  selectedImage?: string;
+};
+
+export default function ImageViewer({ imgSource, selectedImage }: Props) {
+  const imageSourse = selectedImage ? { uri: selectedImage } : imgSource;
+
+  return <Image source={imageSourse} style={styles.image} />;
+}
+
+const styles = StyleSheet.create({
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+});
